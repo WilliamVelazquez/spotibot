d315f23aba10e42d189c8a10f88b8b73a3391560
William Velazquez <william.velazquez.a@gmail.com>
2019-08-17 15:36:16 -0500

    Creating Command to Find Two Tracks and Reply both of them

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ const spotify = require('./usecases/spotify')
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.start(context => {
-  console.log('first_name:',context.from.first_name)
+  // console.log('first_name:',context.from.first_name)
   context.reply(`Hola ${context.from.first_name}! 🎶\nPuedes buscar una canción con el comando\n/track nombreDeLaCanción`)
 })
 
-bot.command('track',(context)=>{
-  // context.message.text
-  const messageText = context.message.text.replace('/track', '').trim()
-  context.reply(messageText)
+bot.command('track', async (context)=>{
+  const query = context.message.text.replace('/track', '').trim()
+  const tracks = await spotify.findTracks(query)
+  // const tracksAsText = tracks.join(' - ')
+  const replyPromises = tracks.map(track=>{context.reply(track)})
+  const replies = await Promise.all(replyPromises)
 })
 
 bot.launch()
